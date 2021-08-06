c0586eee5e71c450ba3257e9e062cde6cd8942d0
Lance Bailey <lancebailey222@gmail.com>
2021-08-05 22:28:26 -0400

    heroku

--- server.js
+++ server.js
@@ -4,19 +4,15 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {maxAge: 36000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -25,8 +21,6 @@ const sess = {
 };
 
 app.use(session(sess));
-
-// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
